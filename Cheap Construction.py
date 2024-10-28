ae8fa764cfd3b29609ed3e48fb10f933ceebe533
rolansy <ronalshoeygeorge@gmail.com>
2024-10-28 22:50:09 +0530

    Refactor suffix array and lcp array construction in Cheap Construction.py

--- Cheap Construction.py
+++ Cheap Construction.py
@@ -1,36 +1,5 @@
-def suffix_array_construction(s):
-    n = len(s)
-    suffix_array = sorted(range(n), key=lambda i: s[i:])
-    rank = [0] * n
-    for i, suffix in enumerate(suffix_array):
-        rank[suffix] = i
-    k = 1
-    while k < n:
-        key = lambda i: (rank[i], rank[i + k] if i + k < n else -1)
-        suffix_array.sort(key=key)
-        tmp = [0] * n
-        for i in range(1, n):
-            tmp[suffix_array[i]] = tmp[suffix_array[i - 1]] + (key(suffix_array[i - 1]) < key(suffix_array[i]))
-        rank = tmp
-        k *= 2
-    return suffix_array
-
-def lcp_array_construction(s, suffix_array):
-    n = len(s)
-    rank = [0] * n
-    for i, suffix in enumerate(suffix_array):
-        rank[suffix] = i
-    lcp = [0] * n
-    h = 0
-    for i in range(n):
-        if rank[i] > 0:
-            j = suffix_array[rank[i] - 1]
-            while i + h < n and j + h < n and s[i + h] == s[j + h]:
-                h += 1
-            lcp[rank[i]] = h
-            if h > 0:
-                h -= 1
-    return lcp
+import sys
+input = sys.stdin.read
 
 class UnionFind:
     def __init__(self, n):
@@ -38,53 +7,43 @@ class UnionFind:
         self.rank = [1] * n
         self.count = n
 
-    def find(self, u):
-        if self.parent[u] != u:
-            self.parent[u] = self.find(self.parent[u])
-        return self.parent[u]
+    def find(self, p):
+        if self.parent[p] != p:
+            self.parent[p] = self.find(self.parent[p])
+        return self.parent[p]
 
-    def union(self, u, v):
-        root_u = self.find(u)
-        root_v = self.find(v)
-        if root_u != root_v:
-            if self.rank[root_u] > self.rank[root_v]:
-                self.parent[root_v] = root_u
-            elif self.rank[root_u] < self.rank[root_v]:
-                self.parent[root_u] = root_v
+    def union(self, p, q):
+        rootP = self.find(p)
+        rootQ = self.find(q)
+        if rootP != rootQ:
+            if self.rank[rootP] > self.rank[rootQ]:
+                self.parent[rootQ] = rootP
+            elif self.rank[rootP] < self.rank[rootQ]:
+                self.parent[rootP] = rootQ
             else:
-                self.parent[root_v] = root_u
-                self.rank[root_u] += 1
+                self.parent[rootQ] = rootP
+                self.rank[rootP] += 1
             self.count -= 1
 
-def find_min_length_for_k_components(s, k):
-    n = len(s)
-    suffix_array = suffix_array_construction(s)
-    lcp = lcp_array_construction(s, suffix_array)
-    
-    uf = UnionFind(n)
-    lcp_with_index = sorted((lcp[i], i) for i in range(1, n))
-    
-    component_count = n
-    for length, i in lcp_with_index:
-        if length == 0:
-            continue
-        if uf.find(suffix_array[i]) != uf.find(suffix_array[i - 1]):
-            uf.union(suffix_array[i], suffix_array[i - 1])
-            component_count -= 1
-        if component_count == k:
-            return length
-    return 0
-
 def main():
-    import sys
-    input = sys.stdin.read
-    s = input().strip()
-    n = len(s)
-    
-    results = []
-    for k in range(1, n + 1):
-        results.append(find_min_length_for_k_components(s, k))
-    
+    S = input().strip()
+    N = len(S)
+    results = [0] * N
+
+    for length in range(1, N + 1):
+        uf = UnionFind(N)
+        seen = set()
+        for i in range(N - length + 1):
+            substring = S[i:i + length]
+            if substring in seen:
+                continue
+            seen.add(substring)
+            for j in range(i, i + length - 1):
+                uf.union(j, j + 1)
+        for k in range(1, N + 1):
+            if results[k - 1] == 0 and uf.count == k:
+                results[k - 1] = length
+
     print(" ".join(map(str, results)))
 
 if __name__ == "__main__":
