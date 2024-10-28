e9b24fe2acf93fbfac99b6eea35be056a0983cdb
rolansy <ronalshoeygeorge@gmail.com>
2024-10-28 22:49:54 +0530

    Refactor star road algorithm for improved performance

--- star road.py
+++ star road.py
@@ -1,39 +1,36 @@
-def dfs(city, stars, graph, memo):
-    if city in memo:
-        return memo[city]
-    
-    max_length = 1  # At least the current city
-    for neighbor in graph[city]:
-        if stars[neighbor] > stars[city]:
-            max_length = max(max_length, 1 + dfs(neighbor, stars, graph, memo))
-    
-    memo[city] = max_length
-    return max_length
+from sys import setrecursionlimit, stdin, stdout
+setrecursionlimit(100000)
 
-def main():
-    import sys
-    input = sys.stdin.read
-    data = input().strip().split()
+def dfs(city, last_star):
+    if dp[city] != -1:
+        return dp[city]
     
-    N = int(data[0])
-    stars = list(map(int, data[1:N+1]))
-    edges = data[N+1:]
+    max_restaurants = 1
+    for neighbor in adj[city]:
+        if stars[neighbor] > last_star:
+            max_restaurants = max(max_restaurants, 1 + dfs(neighbor, stars[neighbor]))
     
-    graph = [[] for _ in range(N)]
-    for i in range(0, len(edges), 2):
-        u = int(edges[i]) - 1
-        v = int(edges[i+1]) - 1
-        graph[u].append(v)
-        graph[v].append(u)
-    
-    global max_restaurants
-    max_restaurants = 0
-    memo = {}
-    
-    for i in range(N):
-        max_restaurants = max(max_restaurants, dfs(i, stars, graph, memo))
-    
-    print(max_restaurants)
+    dp[city] = max_restaurants
+    return dp[city]
 
-if __name__ == "__main__":
-    main()
+# Input
+N = int(input())
+adj = [[] for _ in range(N + 1)]
+stars = [0] * (N + 1)
+dp = [-1] * (N + 1)
+
+# Read stars array
+stars = [0] + list(map(int, input().split()))
+
+# Read edges
+for _ in range(N - 1):
+    u, v = map(int, input().split())
+    adj[u].append(v)
+    adj[v].append(u)
+
+# Calculate maximum path
+max_path = 0
+for i in range(1, N + 1):
+    max_path = max(max_path, dfs(i, -1))
+
+print(max_path)
